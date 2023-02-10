0fb9d4316198e4f1fae0eb99d117c2a8ea3a1526
224043 <224043@edu.rocfriesepoort.nl>
2023-02-10 12:38:00 +0100

    login

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in </title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+
+</head>
+<body>
+
+    <div class="main">
+    
+    <h1> Log in </h1>
+    <br> <br>
+
+    <input type="text" name="username" id="username" placeholder="username"><br>
+    <input type="password" name="password" id="password" placeholder="password"><br>
+    <input type="submit" name="submit" id="submit" value='Log in'><br>
+    
+    <h3> OR </h3><br>
+
+    <a id="login" href="> Register </a>        
+    
+    </div>
+
+     
+</body>
+</html>
