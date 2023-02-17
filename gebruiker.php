19404998dd35ddffb05882a74c0ed9ebc56ce855
RobertoSmit <robertoangelosmit@gmail.com>
2023-02-17 11:57:41 +0100

    Co-authored-by: 224043 <224043@users.noreply.github.com> Co-authored-by: VirusXDragon <VirusXDragon@users.noreply.github.com>

---
+++ gebruiker.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>Gebruiker - De JostibandVirus</title>
+</head>
+<body>
+    <div class="main-gebruiker">
+        <h1>Zieke Docenten</h1>
+        <table>
+            <tr>
+                <th>Voornaam</th>
+                <th>Achternaam</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table> 
+    </div>
+</body>
+</html>
