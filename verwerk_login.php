c7edbce21b4066691b1a6f088d843f59b9c95722
RobertoSmit <robertoangelosmit@gmail.com>
2023-02-24 11:43:23 +0100

    lastonesad

--- verwerk_login.php
+++ verwerk_login.php
@@ -1,22 +1,27 @@
 <?php
+
 include ('connection.php');
+
 $username = filter_input(INPUT_POST,'gebruikersnaam', FILTER_SANITIZE_STRING);
 $password = $_POST['wachtwoord'];
-$query = $db->prepare('SELECT * FROM gebruikers WHERE gebruikersnaam = :user');
+$query = $connect->prepare('SELECT * FROM gebruikers WHERE gebruikersnaam = :user');
 $query->bindParam('user', $username);
 $query->execute();
 
-if ($query->rowCount() == 1) {​
+try
+{
+    if ($query->rowCount() == 1) {​
 
-    $result = $query->fetch(mysqli_connect::FETCH_ASSOC);​
+        $result = $query->fetch(mysqli_connect::FETCH_ASSOC);​
 
-    if(password_verify($password, $result['wachtwoord']))
-    {
-        header('location: gebruiker.php');
+        if(password_verify($password, $result['wachtwoord']))
+        {
+            header('location: gebruiker.php');
+        }
+        else {
+        echo 'Onjuiste gegevens';​
+        }​
     }
-    else {
-      echo 'Onjuiste gegevens';​
-    }​
 }
     catch (mysqli_connect_Exception $e) {​
     die('Error!: ' . $e->getmessage());
