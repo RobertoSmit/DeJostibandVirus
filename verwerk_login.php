7577703524f58481589279de15844216f1320610
224043 <224043@edu.rocfriesepoort.nl>
2023-02-24 10:54:16 +0100

    ..fff

--- verwerk_login.php
+++ verwerk_login.php
@@ -1,25 +1,25 @@
 <?php
 include ('connection.php');
 
-$username = filter_input(INPUT_POST,'gebruiker', FILTER_SANITIZE_STRING);​
-$password = $POST['wachtwoord'];
+$username = filter_input(INPUT_POST,'gebruikersnaam', FILTER_SANITIZE_STRING);
+$password = $_POST['wachtwoord'];
 $query = $db->prepare('SELECT * FROM gebruikers WHERE gebruikersnaam = :user');
 $query->bindParam('user', $username);
-$query->execute();​
+$query->execute();
 
-if($query->rowCount() == 1) {​
+if ($query->rowCount() == 1) {​
 
-    $result = $query->fetch(PDO::FETCH_ASSOC);​
+    $result = $query->fetch(mysqli_connect::FETCH_ASSOC);​
 
     if(password_verify($password, $result['wachtwoord']))
     {
-        header("location: gebruiker.php");
+        header('location: gebruiker.php');
     }
     else {
       echo 'Onjuiste gegevens';​
     }​
 }
-    catch (PDOException $e) {​
+    catch (mysqli_connect_Exception $e) {​
     die('Error!: ' . $e->getmessage());
     }​
 ?>
